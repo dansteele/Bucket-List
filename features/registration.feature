b811992bd62ac0b4dc74f2496d10e2c09af45fdc
Daniel Steele <danielsteele@hotmail.co.uk>
2015-03-09 10:47:08 +0000

    Created welcome controller/view

---
+++ features/registration.feature
@@ -0,0 +1,11 @@
+Feature: Registration
+  
+  @wip
+  Scenario: A traveller registers with the site
+    Given I am on the homepage
+      And I go to the sign-up page
+    When I complete the form with valid details
+      And I submit the form
+    Then I should have an account
+      And I should recieve a welcome email
+      And I should see "Welcome aboard" in the flash notice
---
+++ features/step_definitions/registration_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am on the homepage$/) do
+  visit root_path
+end
+
+Given(/^I go to the sign\-up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I complete the form with valid details$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I submit the form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should have an account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should recieve a welcome email$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" in the flash notice$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
