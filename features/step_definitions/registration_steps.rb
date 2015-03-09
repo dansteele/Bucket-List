4a2503cdebd6bb035a48434b240d09bf34dd638e
Daniel Steele <danielsteele@hotmail.co.uk>
2015-03-09 11:04:17 +0000

    Created steps

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -3,25 +3,30 @@ Given(/^I am on the homepage$/) do
 end
 
 Given(/^I go to the sign\-up page$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link "Sign up"
 end
 
 When(/^I complete the form with valid details$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in "Email", with: "danielsteele@dangarland.co.uk"
+  fill_in "Password", with: "password1"
+  fill_in "Password confirmation", with: "password1"
 end
 
 When(/^I submit the form$/) do
-  pending # express the regexp above with the code you wish you had
+  click_on "Sign up"
 end
 
 Then(/^I should have an account$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(Traveller.count).to eq 1
 end
 
 Then(/^I should recieve a welcome email$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(ActionMailer::Base.deliveries.length). to eq 1
+  expect(ActionMailer::Base.deliveries.first.subject). to match(/Welcome/)
 end
 
-Then(/^I should see "(.*?)" in the flash notice$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see "(.*?)" in the flash notice$/) do |message|
+  within '#flash' do
+    expect(page.has_content? message).to be true
+  end
 end
