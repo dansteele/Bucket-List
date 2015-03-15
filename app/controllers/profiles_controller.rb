b694b9da3688b11736c73b7625aec1ef30461219
Daniel Steele <danielsteele@hotmail.co.uk>
2015-03-15 13:52:10 +0000

    App checks if looking at own profile or another's

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,12 +5,16 @@ class ProfilesController < ApplicationController
   end
 
   def show
-    @traveller = Traveller.find(params[:id])
-    @activities = @traveller.activities.includes(:destination).order(:destination_id)
-    @destination = Destination.new
-    @destinations = Destination.all
-    @traveller_destinations = @traveller.destinations
-    # @destinations = Destination.all
+    if current_traveller.nil? || current_traveller.id != params[:id].to_i
+      @is_viewer = true
+      @traveller = Traveller.find(params[:id])
+      @activities = @traveller.activities.includes(:destination).order(:destination_id)
+      @destination = Destination.new
+      @destinations = Destination.all
+      @traveller_destinations = @traveller.destinations
+    else
+      redirect_to bucket_list_path
+    end
   end
 
 end
